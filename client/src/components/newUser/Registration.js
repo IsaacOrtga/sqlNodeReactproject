b09c23a8aa0f2a26b356ffd97c9d539ff2448661
IsaacOrtga <ortga.isaac@gmail.com>
2023-01-21 15:26:31 +0100

    little changes at form from newUser view

--- client/src/components/newUser/Registration.js
+++ client/src/components/newUser/Registration.js
@@ -4,58 +4,26 @@ import { useNavigate } from "react-router-dom";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-
-
+import './registration.css';
 const Registration = () => {
-
     const navigate = useNavigate()
     const [ name, setName ] = useState('');
     const [ surName, setSurName ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ pass, setPass ] = useState('');
     const [ confirm, setConfirm ] = useState('');
-  
-
-    const regValidation = event => {
-
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                nameReg: name,
-                surNameReg: surName,
-                emailReg: email,
-                passReg: pass,
-                confirmPass: confirm,
-            }),
-        };
-        
-        fetch("user", requestOptions)
-        .then((response) => response.json())
-        .then((res) => {
-
-            if (res.user) {
-            localStorage.setItem('email', email);
-                        alert('El registro se ha realizado con éxito. Bienvenid@')
-                        navigate('/')
+    const createNewUser = async () =>{
+        const connectionExpress = await fetch({
+            method: 'POST',
+            body: JSON.stringify(),
+            headers: {
 
             }
-            else {
-
-                alert('Datos no válidos')
-
-            }
-            
-
-
-        })
+        });
+        return await connectionExpress.json();
     }
-
-
-
-
     function showPass() {
-        var tipo = document.getElementById("pass");
+        var tipo = document.getElementById("pass1");
         var tipo2 = document.getElementById("pass2");
         if (tipo.type === "password" || tipo2.type === "password") {
             tipo.type = "text";
@@ -65,92 +33,23 @@ const Registration = () => {
             tipo2.type = "password"
         }
     };
-
     return (
-        <div className='regContainer' style={{ border: "1px solid black", height: 520, margin: "0 auto", width: "28%" }}>
+        <div className='generalContainerRegistration'>
+        <div className='regContainer'>
+        <h3>Bienvenido a la comunidad de <span>Plans</span></h3>
 
-            <FloatingLabel
-                style={{ width: 300 }}
-                controlId="floatingInput"
-                label="Nombre"
-                className="mb-3 mt-4 ms-4">
-                <Form.Control
-                    type="text"
-                    placeholder="Nombre"
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-    
-            </FloatingLabel>
-
-            <FloatingLabel
-                style={{ width: 300 }}
-                controlId="floatingInput"
-                label="Apellidos"
-                className="mb-3 mt-4 ms-4">
-                <Form.Control
-                    type="text"
-                    placeholder="Apellidos"
-                    onChange={(e) => setSurName(e.target.value)}
-
-
-                />
-            </FloatingLabel>
-
-            <FloatingLabel
-                style={{ width: 300 }}
-                controlId="floatingInput"
-                label="Email"
-                className="mb-3 mt-4 ms-4">
-                <Form.Control
-                    type="email"
-                    placeholder="name@example.com"
-                    onChange={(e) => setEmail(e.target.value)}
-
-
-                />
-            </FloatingLabel>
-
-            <FloatingLabel
-                style={{ width: 300 }}
-                controlId="floatingInput"
-                label="Contraseña"
-                className="mb-3 mt-4 ms-4">
-                <Form.Control
-                    id='pass'
-                    type="password"
-                    placeholder="Contraseña"
-                    onChange={(e) => setPass(e.target.value)}
-
-
-                />
-            </FloatingLabel>
-
-            <FloatingLabel
-                style={{ width: 300 }}
-                controlId="floatingPassword"
-                label="Confirmar contraseña"
-                className="mb-3 mt-4 ms-4">
-                <Form.Control
-                    id='pass2'
-                    type="password"
-                    placeholder="Contraseña"
-                    onChange={(e) => setConfirm(e.target.value)}
-
-
-                />
-            </FloatingLabel>
-
-            <Form.Check type="checkbox" label="Mostrar contraseña" className='ms-4 mt-2' onClick={showPass} />
-
-
-            <Button className='ms-4 mt-2' variant="success" size="sm" type="submit" style={{ width: "30%" }} onClick={ regValidation }>
-                Iniciar sesión
-            </Button>
-
+            <input type="text" name="name" placeholder='nombre' className="inputRegistration"></input>
+            <input type="text" name="surName" placeholder='apellidos' className="inputRegistration"></input>
+            <input type="text" name="nick" placeholder='alias' className="inputRegistration"></input>
+            <input type="email" name="email" placeholder='e-mail' className="inputRegistration"></input>
+            <input type="password" name="pass" placeholder='contraseña' className="inputRegistration" id="pass1"></input>
+            <input type="password" name="confirmPass" placeholder='repetir contraseña' className="inputRegistration" id="pass2"></input>
+            <Form.Check type="checkbox" label="Mostrar contraseña" className='checkPass ms-4 mt-2' onClick={showPass} />
+            <Button className='button ms-4 mt-2' variant="success" size="sm" type="submit" style={{ width: "30%" }} onClick={ regValidation }>
+                Enviar
+            </Button> 
+        </div>
         </div>
     )
-}
-
-
+};
 export default Registration;
--- client/src/pages/NewUser.js
+++ client/src/pages/NewUser.js
@@ -8,7 +8,7 @@ const NewUser = () => {
     <div>
         <Navbar />
         <Registration />
-        <Footer />
+        {/* <Footer /> */}
     </div>
   )
 }
