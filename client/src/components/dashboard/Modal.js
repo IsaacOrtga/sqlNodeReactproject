b7529fcb250f7bcf63fcbb9f4744fbe32e5df277
IsaacOrtga <ortga.isaac@gmail.com>
2023-02-13 21:15:27 +0100

    finished views of dashboard and modal

--- client/src/components/dashboard/Modal.js
+++ client/src/components/dashboard/Modal.js
@@ -22,20 +22,25 @@ function Modal({ showModal, children, onClose }) {
                 &times;
               </button>
             </div>
-            <div className="modalBody">{children}</div>
-            <label for="descriptionTextArea">Descríbete: </label>
-            <textarea name="descriptionTextArea" className="modalTextArea" placeholder="Descríbete en 500 palabras" value={description} onChange={handleChange} />
-            <label for="interestsTextArea">Escoge tus intereses:
-              <select name="interestsTextArea" value={interests} onChange={handleChange}>
-                <option value="art">Arte</option>
-                <option value="music">Música</option>
-                <option value="books">Libros</option>
-                <option value="vegan">Vegano</option>
-                <option value="food">Comida</option>
-                <option value="fashion">Moda</option>
-              </select>
-            </label>
-            <input type="submit" value="Submit" />
+            <div className="modalBody">
+              <div className="descriptionContainer">
+                <label for="descriptionTextArea"><b>Descríbete:</b> </label>
+                <textarea maxlength="500" name="descriptionTextArea" className="modalTextArea" placeholder="500 palabras" value={description} onChange={handleChange} />
+              </div>
+              <div className="interestsContainer">
+                <label for="interestsTextArea"><b>Escoge tus intereses: </b>
+                  <select name="interestsSelector" value={interests} onChange={handleChange}>
+                    <option value="art">Arte</option>
+                    <option value="music">Música</option>
+                    <option value="books">Libros</option>
+                    <option value="vegan">Vegano</option>
+                    <option value="food">Comida</option>
+                    <option value="fashion">Moda</option>
+                  </select>
+                </label>
+              </div>
+              <input type="submit" value="Guardar" id="submitModal" className="submitModal" onClick={handleChange} />
+            </div>
           </div>
         </div>
       )}
